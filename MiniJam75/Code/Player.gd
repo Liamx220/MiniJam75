3334d28c806fac7dabc0a0b7a50a01538fb2b4b3
Liam Hohertz <73028237+Liamx220@users.noreply.github.com>
2021-03-05 17:06:21 -0500

    Fixed player movemnt

--- MiniJam75/Code/Player.gd
+++ MiniJam75/Code/Player.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
 
-export (int) var speed = 100
+export (int) var speed = 400
 export (int) var jump_speed = -150
 export (int) var gravity = 300
 
-var velocity = Vector2.ZERO
+var velocity = Vector2()
 
 func get_input():
 	velocity.x = 0
 	if Input.is_action_pressed("walk_right"):
-		velocity.x -= speed
+		velocity.x -= 100
 		$AnimatedSprite.play("leftWalk")
 	if Input.is_action_pressed("walk_left"):
-		velocity.x += speed
+		velocity.x += 100
 		$AnimatedSprite.play("rightWalk")
 
 func _physics_process(delta):
@@ -22,3 +22,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("enemy"):
+		get_tree().reload_current_scene()
