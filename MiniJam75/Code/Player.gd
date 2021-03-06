523c0f42c0a39499028608653c49335a33d2e327
Liam Hohertz <73028237+Liamx220@users.noreply.github.com>
2021-03-06 11:30:43 -0500

    update

--- MiniJam75/Code/Player.gd
+++ MiniJam75/Code/Player.gd
@@ -3,13 +3,16 @@ extends KinematicBody2D
 export (int) var speed = 400
 export (int) var jump_speed = -150
 export (int) var gravity = 300
-export (int) var weight_int = 500
+onready var weight_int_num = get_node("/root/Weight")
 onready var weight_text = get_node("Weight")
 
 var velocity = Vector2()
 
 func _ready():
-	weight_text.text = "Weight: " + str(weight_int)
+	
+	
+	weight_text.text = "Weight: " + str(weight_int_num.Weight)
+	
 
 func get_input():
 	velocity.x = 0
@@ -22,7 +25,8 @@ func get_input():
 		$AnimatedSprite.play("rightWalk")
 
 func _physics_process(delta):
-	weight_text.text = "Weight: " + str(weight_int)
+	var weight_text = get_node("Weight")
+	weight_text.text = "Weight: " + str(weight_int_num.Weight)
 	get_input()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -33,7 +37,14 @@ func _physics_process(delta):
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("enemy"):
-		get_tree().reload_current_scene() 
+		get_tree().reload_current_scene()
 	if area.is_in_group("food"):
-		weight_int -= 20
+		weight_int_num.Weight -= 5
 		area.queue_free()
+	if area.is_in_group("door"):
+		get_tree().change_scene("res://Scenes/Lvls/2.tscn")
+
+
+func _on_CollisionShape2D_tree_entered(area):
+	if area.is_in_group("enemy"):
+		get_tree().reload_current_scene()
---
+++ MiniJam75/weight.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+var Weight = 500
+
+func _ready():
+	var Weight = 500
+
+
+
+
+
+
